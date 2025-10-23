116c9038db853be30cd36f4516ba8a42111f83d6
Aman Kumar <maurya001aman@gmail.com>
2025-10-23 19:54:11 +0530

    Initial Commit

---
+++ src/main/java/com/impactai/impactai/ImpactAiApplication.java
@@ -0,0 +1,13 @@
+package com.impactai.impactai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/impactai/impactai/ImpactAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impactai.impactai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
